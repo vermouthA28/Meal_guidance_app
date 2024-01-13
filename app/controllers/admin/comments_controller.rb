3a6f5b099a217a91e75524a140fc46b684a598eb
vermouthA28 <147418988+vermouthA28@users.noreply.github.com>
2024-01-13 15:40:43 +0000

    fix:コメントのフォームは出せるようにしました

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,18 +1,31 @@
 class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
   
+  
+  # def create
+  #   post = Post.find(params[:post_id])
+  #   if current_user.admin?
+  #     @comment = Comment.new(comment_params)
+  #     @comment.post_id = post.id
+  #     @comment.save
+  #     redirect_to request.referer
+  #   else
+  #     redirect_to root_path, notice: "管理者以外はコメントを作成できません。"
+  #   end
+  # end
+  
   def create
-    post = Post.find(params[:post_id])
-    if current_user.admin?
+    post = Post.find_by(id: params[:post_id])
+    if post
       @comment = Comment.new(comment_params)
-    # comment = post.comments.new(comment_params)
-      comment.post_id = post.id
-      comment.save
-      redirect_to request.referer
+      @comment.post_id = post.id
+      @comment.save
+    redirect_to request.referer
     else
-      redirect_to root_path, notice: "管理者以外はコメントを作成できません。"
+      # エラー処理またはログ出力など、投稿が見つからなかった場合の対応を行う
+      flash[:error] = "投稿が見つかりませんでした。"
+      redirect_to request.referer
     end
-    
   end
   
   def destroy
